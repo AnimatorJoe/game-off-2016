d914dcb114fe9d76af4183b52191902ed729bf8a
Eli Joesph Bradley <elijos03@gmail.com>
2016-11-24 20:33:08 -0700

    Remove unnecessary cast from UInt32 to Int

--- Shared/GameScene.swift
+++ Shared/GameScene.swift
@@ -204,7 +204,7 @@ class GameScene: SKScene {
             enemy.textureArray = textureMatrix[enemyNumber]
         
             // Add enemy to scene
-            switch Int(arc4random_uniform(2)) {
+            switch arc4random_uniform(2) {
                 // Move from top to bottom
                 case 0:
                     enemy.position = CGPoint(x: self.size.width/2 - CGFloat(arc4random_uniform(UInt32(self.size.width))),
@@ -213,7 +213,7 @@ class GameScene: SKScene {
                                                 y: (self.size.height * -3/5) - enemy.position.y,
                                                 duration: 15.0 + Double(arc4random_uniform(10)))
                 
-                //Move from bottom to top
+                // Move from bottom to top
                 case 1:
                     enemy.position = CGPoint(x: self.size.width/2 - CGFloat(arc4random_uniform(UInt32(self.size.width))),
                                              y: self.size.height * -0.55 + CGFloat(arc4random_uniform(UInt32(self.size.height/9))))
@@ -221,7 +221,7 @@ class GameScene: SKScene {
                                                 y: (self.size.height * 3/5) - enemy.position.y,
                                                 duration: 15.0 + Double(arc4random_uniform(10)))
                 
-                //In case of error
+                // In case arc4random_uniform(u_int32_t upper_bound); fails
                 default:
                     enemy.position = CGPoint(x: self.size.width/2 - CGFloat(arc4random_uniform(UInt32(self.size.width))),
                                              y: self.size.height * 0.55 + CGFloat(arc4random_uniform(UInt32(self.size.height/9))))
