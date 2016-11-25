b4757f5941662b6364c24a52ba6e51ff98034c51
Eli Joesph Bradley <elijos03@gmail.com>
2016-11-25 11:37:41 -0700

    Organization

--- Shared/GameScene.swift
+++ Shared/GameScene.swift
@@ -191,8 +191,8 @@ class GameScene: SKScene {
         for enemy in enemyArray {
             if enemy!.intersects(badGuys!) {
                 if badGuys!.particleBirthRate * 5 < enemy!.health {
-                    badGuys?.particleBirthRate *= 0.999
                     enemy?.deteriorate()
+                    badGuys?.particleBirthRate *= 0.999
                 } else {
                     enemy?.deteriorate()
                     badGuys?.particleBirthRate += enemy!.health * (1 - enemy!.deteriorationRate)
