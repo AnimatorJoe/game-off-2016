2bff4f4b70e49fb5f0dfcb763e6a0c344d07e7ed
AnimatorJoe <josephjin7@gmail.com>
2016-11-24 18:32:56 -0600

    Added more enemy sizes

--- Shared/GameScene.swift
+++ Shared/GameScene.swift
@@ -194,6 +194,7 @@ class GameScene: SKScene {
         let waitRandom = SKAction.wait(forDuration: TimeInterval(arc4random_uniform(UInt32(2))))
         let enemy = SKEnemyNode(texture: textureMatrix[enemyNumber][0])
         let randomTime = 15 + Int(arc4random_uniform(10))
+        let scale = CGFloat(UInt32(5) + (arc4random_uniform(UInt32(3))))/10
         
         enemy.textureArray = textureMatrix[enemyNumber]
         
@@ -203,8 +204,8 @@ class GameScene: SKScene {
         let moveEnemy = SKAction.moveBy(x: CGFloat((self.size.width/2) - CGFloat(arc4random_uniform(UInt32(self.size.width)))) - enemy.position.x,
                                         y: (self.size.height * -3/5) - enemy.position.y, duration: Double(randomTime))
         enemy.zPosition = 2
-        enemy.xScale = 0.6
-        enemy.yScale = 0.6
+        enemy.xScale = scale
+        enemy.yScale = scale
         self.addChild(enemy)
         
         enemyArray.append(enemy)
