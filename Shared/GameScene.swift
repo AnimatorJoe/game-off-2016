7ebc0d077344c62a73a24b9eaa9326a0bfd647d3
Eli Joesph Bradley <elijos03@gmail.com>
2016-11-26 15:24:59 -0700

    Fix building on other platforms, update Help page with text from README.md.

--- Shared/GameScene.swift
+++ Shared/GameScene.swift
@@ -250,7 +250,7 @@ class GameScene: SKScene {
         enemyArray.append(enemy)
         
         // Spawn more enemies
-        enemy.run(SKAction.sequence([moveEnemy, waitRandom, SKAction.removeFromParent()]), completion: {self.spawnEnemies()})
+        enemy.run(SKAction.sequence([moveEnemy, waitRandom, SKAction.removeFromParent()]), completion: self.spawnEnemies)
     }
     
     // MARK: Remove Enemies Off Screen
